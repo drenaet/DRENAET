4afc1d01051598c80cbe0092a89e13cf0852e749
drenaet <rogercopernic@gmail.com>
2026-06-22 20:51:08 +0000

    Update emoji in annonces to clipboard icon

--- data.js
+++ data.js
@@ -77,7 +77,7 @@ const DATA = {
       {
         type:    "info",
         couleur: "blue",
-        emoji:   "photos/preview.jpg",
+        emoji:   "📋",
         titre:   "Bienvenue sur le portail DRENAET Agboville",
         texte:   "Plateforme officielle de la Direction Régionale de l'Éducation Nationale de l'alphabétisation et de l'Enseignement Technique d'Agboville. Toutes les informations sur les établissements et le personnel.",
         date:    "Juin 2026"
